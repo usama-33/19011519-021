97b5681df1f879e701bfea526ab70aaa701b553d
Abdulrehman usama <chusama33@gmail.com>
2020-08-20 16:30:27 +0500

    final term

---
+++ src/main/java/com/finalterm/solution/FinaltermApplication.java
@@ -0,0 +1,13 @@
+package com.finalterm.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaltermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalterm/solution/FinaltermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalterm.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaltermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
